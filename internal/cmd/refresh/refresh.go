d163857aa2aa8082a2b937409c3e248b7fdf67df
Michal Bock <michal.bock@gmail.com>
2018-09-11 14:30:31 +0200

    Add the ability to send SIGINT and SIGTERM to the main process and random sleep before sending the signal.

--- internal/cmd/refresh/refresh.go
+++ internal/cmd/refresh/refresh.go
@@ -27,4 +27,18 @@ var Flags = []cli.Flag{
 		Usage: "Substring of a command used to run the executable that" +
 			"should be signalled to when a new certificate is retrieved.",
 	},
+	cli.StringFlag{
+		Name:   "signal",
+		EnvVar: "SIGNAL",
+		Value:  "SIGHUP",
+		Usage: "Signal to be send to the main process, when the certificates are refreshed. " +
+			`Allowed values are "SIGHUP", "SIGTERM" and "SIGINT".`,
+	},
+	cli.DurationFlag{
+		Name:   "max-random-sleep",
+		EnvVar: "MAX_RANDOM_SLEEP",
+		Value:  time.Duration(0),
+		Usage: "Maximum random sleep time before sending the signal to the main process. " +
+			"This is to prevent all containers being restarted at the same time.",
+	},
 }
